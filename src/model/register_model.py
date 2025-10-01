bdec4a0712dc78cc1e172bd60a7b66a710e64408
sandeepdash-mlops <sandeepdash@gmail.com>
2025-10-01 10:41:55 +0000

    CICD 4

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -79,7 +79,7 @@ def register_model(model_name: str, model_info: dict):
         client.transition_model_version_stage(
             name=model_name,
             version=model_version.version,
-            stage="Production"
+            stage="Staging"
         )
         
         logging.debug(f'Model {model_name} version {model_version.version} registered and transitioned to Staging.')
